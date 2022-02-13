290be5b8fe03fc44ca877f981a096c79dda4445e
haithem ghattas <haithem.ghattas@esprit.tn>
2022-02-13 16:13:10 +0100

    Initial commit

---
+++ zvenn/migrations/Version20220212164425.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220212164425 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE produits MODIFY id_restaurant INT NOT NULL');
+        $this->addSql('ALTER TABLE produits DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE produits CHANGE id_restaurant id_produit INT AUTO_INCREMENT NOT NULL');
+        $this->addSql('ALTER TABLE produits ADD PRIMARY KEY (id_produit)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE produits MODIFY id_produit INT NOT NULL');
+        $this->addSql('ALTER TABLE produits DROP PRIMARY KEY');
+        $this->addSql('ALTER TABLE produits CHANGE nom_produit nom_produit VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description_produit description_produit VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE type_produit type_produit VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE id_produit id_restaurant INT AUTO_INCREMENT NOT NULL');
+        $this->addSql('ALTER TABLE produits ADD PRIMARY KEY (id_restaurant)');
+    }
+}
